05fd650f80f06494ee4346025f786c4e5fa8e8b8
jdecter2030-create <jdecter2030@mka.org>
2026-04-23 12:10:57 -0400

    Trouble

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ import {
 
 export default function DetailingBusinessWebsite() {
   const business = {
-    name: "Elite Auto Detailing",
+    name: "Elite Auto Detailng",
     tagline:
       "Professional interior and exterior car detailing that makes your vehicle look brand new.",
     phoneDisplay: "(973) 464-5222",
